5889fd765cb00b7292171d32eddaa9d69a63783b
Eidy Trinidad <Eidyjtf@gmail.com>
2023-08-04 16:15:55 -0400

    component compound listo

---
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -0,0 +1,34 @@
+import { useProduct } from "../hooks/useProduct";
+import { createContext } from "react";
+import {
+  ProductCardProps,
+  ProductContextProps,
+} from "../interfaces/interfaces";
+import styles from "../styles/styles.module.css";
+
+const initContextState: ProductContextProps = {
+  counter: 0,
+  increaseBy: (value: number) => {},
+  product: {
+    id: "",
+    title: "",
+  },
+};
+export const ProductContext = createContext(initContextState);
+const { Provider } = ProductContext;
+
+export const ProductCard = ({ children, product }: ProductCardProps) => {
+  const { counter, increaseBy } = useProduct();
+
+  return (
+    <Provider
+      value={{
+        counter,
+        increaseBy,
+        product,
+      }}
+    >
+      <div className={styles.productCard}>{children}</div>
+    </Provider>
+  );
+};
---
+++ src/02-component-patterns/components/index.ts
@@ -0,0 +1,18 @@
+import { ProductCard as ProductcardHOC } from "./ProductCard";
+
+import { ProductButtons } from "./ProductButtons";
+import { ProductImage } from "./ProductImage";
+import { ProductTitle } from "./ProductTitle";
+import { ProductCardHOCProps } from "../interfaces/interfaces";
+
+export * from "./ProductButtons";
+export * from "./ProductImage";
+export * from "./ProductTitle";
+
+export const ProductCard: ProductCardHOCProps = Object.assign(ProductcardHOC, {
+  Title: ProductTitle,
+  Image: ProductImage,
+  Buttons: ProductButtons,
+});
+
+export default ProductCard;
